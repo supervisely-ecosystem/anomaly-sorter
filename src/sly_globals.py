cb6af0d98a83eed9a461b33d2ed4a7f8858cebf4
almaz <almazgimaev@gmail.com>
2025-06-18 16:47:28 +0200

    Update local.env and sly_globals.py to ensure TASK_ID is set and dataset_id is loaded in development environment

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -7,10 +7,13 @@ import supervisely as sly
 if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
+    # task_id = sly.app.development.enable_advanced_debug(team_id=sly.env.team_id())
+
 
 api = sly.Api.from_env()
 team_id = sly.env.team_id()
 project_id = sly.env.project_id()
+dataset_id = sly.env.dataset_id(raise_not_found=False)
 
 target_class = "person"
 
